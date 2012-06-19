a3ec900bfc21215f566bfaebd29a21ec9a83ed0f
Bruno Lowagie <bruno.lowagie@itextpdf.com>
2012-06-19 16:14:16 +0000

    Preparing for RUPS 1.0.2

--- src/main/java/com/itextpdf/rups/Rups.java
+++ src/main/java/com/itextpdf/rups/Rups.java
@@ -91,7 +91,7 @@ public class Rups {
     
 	// other member variables
 
-	private static final String RUPS_VERSION = "1.0.2-SNAPSHOT";
+	private static final String RUPS_VERSION = "1.0.2";
 	private static File f;
 
 }
