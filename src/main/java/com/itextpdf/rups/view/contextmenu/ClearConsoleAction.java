2d77186464c4316844f18e9537182c4a25149b39
Bruno Lowagie <bruno.lowagie@itextpdf.com>
2013-09-12 09:33:11 +0000

    Adding file headers

--- src/main/java/com/itextpdf/rups/view/contextmenu/ClearConsoleAction.java
+++ src/main/java/com/itextpdf/rups/view/contextmenu/ClearConsoleAction.java
@@ -1,3 +1,22 @@
+/*
+ * $Id:  $
+ *
+ * Copyright 2007 Bruno Lowagie.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
 package com.itextpdf.rups.view.contextmenu;
 
 import javax.swing.*;
@@ -11,7 +30,10 @@ import java.awt.event.ActionEvent;
  */
 public class ClearConsoleAction extends AbstractRupsAction {
 
-    public ClearConsoleAction(String name) {
+   /** Serial version uid. */
+	private static final long serialVersionUID = -7021881171745264584L;
+
+	public ClearConsoleAction(String name) {
         super(name);
     }
 
